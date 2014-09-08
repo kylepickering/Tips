a3db1531dafbbedb16fc540510a35aa8b562adfa
Kyle Pickering <kyle@airbnb.com>
2014-09-07 18:13:48 -0700

    building initial project

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        billField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.15, 0.2, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tipAmount = billAmount * tipPercentage
+        var totalAmount = billAmount + tipAmount
+        
+        tipLabel.text = "$\(tipAmount)"
+        totalLabel.text = "$\(totalAmount)"
+        
+        tipLabel.text = String(format:"$%.2f", tipAmount)
+        totalLabel.text = String(format:"$%.2f", totalAmount)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
